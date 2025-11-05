bd2559abf7f5f6d1720bf0dfba04bc31c178528c
Alfito Nur Fadhila <88030166+SeniorGit@users.noreply.github.com>
2025-11-05 16:20:34 +0700

    ForgotPassword and Migration

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('./lib/utils/database')
 const {authRouter} = require("./routes/authRouter")
+
 const app = express();
 const port = 3001;
 
